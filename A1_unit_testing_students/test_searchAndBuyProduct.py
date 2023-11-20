0b6adec6d3d4055920424919988bfb36f8c3e3a9
wibr7068 <71329264+wibr7068@users.noreply.github.com>
2023-11-20 17:20:13 +0100

    First stub mocker test finished

--- A1_unit_testing_students/test_searchAndBuyProduct.py
+++ A1_unit_testing_students/test_searchAndBuyProduct.py
@@ -3,15 +3,6 @@ from unittest.mock import patch
 from products import searchAndBuyProduct
 
 @pytest.fixture
-def input_stub(mocker):
-    def input_side_effect(prompt):
-        if "Search for products" in prompt:
-            return "all"
-        elif "Ready to shop" in prompt:
-            return "Y"
-    return mocker.patch('builtins.input', side_effect=input_side_effect)
-
-@pytest.fixture
 def login_stub(mocker):
     return mocker.patch('products.login', return_value={"username": "Ramanathan", "wallet": 100 })
 
@@ -23,10 +14,17 @@ def checkoutAndPayment_stub(mocker):
 def display_csv_as_table_stub(mocker):
     return mocker.patch('products.display_csv_as_table', return_value=None)
 
-@pytest.mark.parametrize("user_inputs", [("Ramanathan", "Notaproblem23*", "all", "Y", "l")])
-def test_with_stub(input_stub, login_stub, checkoutAndPayment_stub, display_csv_as_table_stub, user_inputs):
+@pytest.fixture
+def display_filtered_table_stub(mocker):
+    return mocker.patch('products.display_filtered_table', return_value=None)
+
+@pytest.mark.parametrize("user_inputs", [("all", "Y")])
+def test_with_stubs1(login_stub, checkoutAndPayment_stub, display_csv_as_table_stub, display_filtered_table_stub, user_inputs):
     with patch('builtins.input', side_effect=user_inputs):
         searchAndBuyProduct()
 
-    checkoutAndPayment_stub.assert_called_once_with({"username": "Ramanathan", "wallet": 100 })
     login_stub.assert_called_once()
+    display_csv_as_table_stub.assert_called_once()
+    display_filtered_table_stub.assert_not_called()
+    checkoutAndPayment_stub.assert_called_once_with({"username": "Ramanathan", "wallet": 100 })
+

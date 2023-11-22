530a98a7c6b7bc377c42be6bd94e26a3a7a24d86
JannikRecklies <jannik.recklies@gmx.de>
2023-11-22 14:43:03 +0100

    Assert that json.dump is called in login

--- A1_unit_testing_students/test_login.py
+++ A1_unit_testing_students/test_login.py
@@ -5,6 +5,14 @@ from login import login
 
 
 @pytest.fixture
+def json_dump_mock(monkeypatch):
+    # Create a MagicMock for json.dump
+    mock_dump = mock.MagicMock()
+    monkeypatch.setattr('json.dump', mock_dump)
+    return mock_dump
+
+
+@pytest.fixture
 def registered_user():
     return {"username": "testuser", "password": "Test_Password", "wallet": 0}
 
@@ -19,10 +27,12 @@ def login_open_users_file_stub(monkeypatch, registered_user):
 @pytest.mark.parametrize("user_inputs", [
     ("testuser", "Test_Password")
 ])
-def test_successful_login(user_inputs, login_open_users_file_stub, capsys):
+def test_successful_login(user_inputs, login_open_users_file_stub, json_dump_mock, capsys):
     # Run the login function
     with mock.patch('builtins.input', side_effect=user_inputs):
         login()
+
+    json_dump_mock.assert_not_called()
 
     # Capture the printed output
     captured = capsys.readouterr()
@@ -37,10 +47,12 @@ def test_successful_login(user_inputs, login_open_users_file_stub, capsys):
     ("testuser", "", "n"),
     ("", "Test_Password", "n")
 ])
-def test_failed_login_and_dont_register(user_inputs, login_open_users_file_stub, capsys):
+def test_failed_login_and_dont_register(user_inputs, login_open_users_file_stub, json_dump_mock, capsys):
     # Run the login function
     with mock.patch('builtins.input', side_effect=user_inputs):
         login()
+
+    json_dump_mock.assert_not_called()
 
     # Capture the printed output
     captured = capsys.readouterr()
@@ -53,10 +65,12 @@ def test_failed_login_and_dont_register(user_inputs, login_open_users_file_stub,
     ("non_existing_user", "password", "Y", "Correct_New_Password"),
     ("non_existing_user", "", "Y", "8Letter$")
 ])
-def test_failed_login_and_register_successfully(user_inputs, login_open_users_file_stub, capsys):
+def test_failed_login_and_register_successfully(user_inputs, login_open_users_file_stub, json_dump_mock, capsys):
     # Run the login function
     with mock.patch('builtins.input', side_effect=user_inputs):
         login()
+
+    json_dump_mock.assert_called_once()
 
     # Capture the printed output
     captured = capsys.readouterr()
@@ -72,10 +86,12 @@ def test_failed_login_and_register_successfully(user_inputs, login_open_users_fi
     ("non_existing_user", "password", "Y", "7L%tter", "Correct_New_Password"),
     ("non_existing_user", "password", "Y", "password_missing_capital_letter", "PasswordMissingSpecialCharacter", "Pw$hort", "Correct_New_Password")
 ])
-def test_failed_login_and_retry_register(user_inputs, login_open_users_file_stub, capsys):
+def test_failed_login_and_retry_register(user_inputs, login_open_users_file_stub, json_dump_mock, capsys):
     # Run the login function
     with mock.patch('builtins.input', side_effect=user_inputs):
         login()
+
+    json_dump_mock.assert_called_once()
 
     # Capture the printed output
     captured = capsys.readouterr()
@@ -84,3 +100,4 @@ def test_failed_login_and_retry_register(user_inputs, login_open_users_file_stub
     assert "Either username or password were incorrect" in captured.out
     assert "Password must have at least 1 capital letter, 1 special symbol and be 8 characters long." in captured.out
     assert "Successfully registered" in captured.out
+

8f033ab3b2796ba843086dde13dbd98a45d78e4a
Jannik Recklies <Recklies@student-238-47.eduroam.uu.se>
2023-11-22 13:52:05 +0100

    Added tests for logout

--- A1_unit_testing_students/test_login.py
+++ A1_unit_testing_students/test_login.py
@@ -1,12 +1,12 @@
 import json
 import pytest
 from unittest import mock
-from A1_unit_testing_students.login import login
+from login import login
 
 
 @pytest.fixture
 def registered_user():
-    return {"username": "testuser", "password": "testpassword", "wallet": 0}
+    return {"username": "testuser", "password": "Test_Password", "wallet": 0}
 
 
 @pytest.fixture
@@ -17,7 +17,7 @@ def login_open_users_file_stub(monkeypatch, registered_user):
 
 
 @pytest.mark.parametrize("user_inputs", [
-    ("testuser", "testpassword")
+    ("testuser", "Test_Password")
 ])
 def test_successful_login(user_inputs, login_open_users_file_stub, capsys):
     # Run the login function
@@ -33,8 +33,9 @@ def test_successful_login(user_inputs, login_open_users_file_stub, capsys):
 
 @pytest.mark.parametrize("user_inputs", [
     ("testuser", "wrongpassword", "N"),
-    ("wronguser", "testpassword", "N"),
-    ("", "", "n")
+    ("wronguser", "Test_Password", "N"),
+    ("testuser", "", "n"),
+    ("", "Test_Password", "n")
 ])
 def test_failed_login_and_dont_register(user_inputs, login_open_users_file_stub, capsys):
     # Run the login function
@@ -49,7 +50,8 @@ def test_failed_login_and_dont_register(user_inputs, login_open_users_file_stub,
 
 
 @pytest.mark.parametrize("user_inputs", [
-    ("non_existing_user", "password", "Y", "Correct_New_Password")
+    ("non_existing_user", "password", "Y", "Correct_New_Password"),
+    ("non_existing_user", "", "Y", "8Letter$")
 ])
 def test_failed_login_and_register_successfully(user_inputs, login_open_users_file_stub, capsys):
     # Run the login function
@@ -65,6 +67,9 @@ def test_failed_login_and_register_successfully(user_inputs, login_open_users_fi
 
 
 @pytest.mark.parametrize("user_inputs", [
+    ("non_existing_user", "password", "Y", "password_missing_capital_letter", "Correct_New_Password"),
+    ("non_existing_user", "password", "Y", "PasswordMissingSpecialCharacter", "Correct_New_Password"),
+    ("non_existing_user", "password", "Y", "7L%tter", "Correct_New_Password"),
     ("non_existing_user", "password", "Y", "password_missing_capital_letter", "PasswordMissingSpecialCharacter", "Pw$hort", "Correct_New_Password")
 ])
 def test_failed_login_and_retry_register(user_inputs, login_open_users_file_stub, capsys):
@@ -79,5 +84,3 @@ def test_failed_login_and_retry_register(user_inputs, login_open_users_file_stub
     assert "Either username or password were incorrect" in captured.out
     assert "Password must have at least 1 capital letter, 1 special symbol and be 8 characters long." in captured.out
     assert "Successfully registered" in captured.out
-
-
---
+++ A1_unit_testing_students/test_logout.py
@@ -0,0 +1,107 @@
+import copy
+import pytest
+from unittest import mock
+from logout import logout
+from checkout_and_payment import ShoppingCart, Product
+
+
+def create_expected_output(cart):
+    expected_output = "Your cart is not empty.You have following items\n"
+    for i in cart.retrieve_item():
+        product = i.get_product()
+        expected_output += f"['{product[0]}', {product[1]}, {product[2]}]\n"
+    return expected_output
+
+
+@pytest.fixture
+def cart_empty():
+    return ShoppingCart()
+
+
+@pytest.fixture
+def cart_with_one_element():
+    cart = ShoppingCart()
+    cart.add_item(Product(name="Apple", price=5, units=10))
+    return cart
+
+
+@pytest.fixture
+def cart_with_multiple_elements():
+    cart = ShoppingCart()
+    cart.add_item(Product(name="Apple", price=5, units=10))
+    cart.add_item(Product(name="Banana", price=10, units=2))
+    cart.add_item(Product(name="Orange", price=12, units=5))
+    return cart
+
+
+@pytest.fixture
+def clear_cart_stub(mocker):
+    return mocker.patch('checkoutAndPayment.ShoppingCart.clear_items')
+
+
+def test_logout_with_empty_cart(cart_empty):
+    result = logout(cart=cart_empty)
+
+    assert result == True
+    assert len(cart_empty.items) == 0
+
+
+@pytest.mark.parametrize("user_inputs", ["N", "n", "No", "no"])
+def test_cancel_logout_cart_with_one_element(user_inputs, cart_with_one_element, capsys):
+    copy_cart = copy.deepcopy(cart_with_one_element)
+
+    with mock.patch('builtins.input', side_effect=user_inputs):
+        result = logout(cart=cart_with_one_element)
+
+    # Capture the printed output
+    captured = capsys.readouterr()
+
+    assert result == False
+    assert len(cart_with_one_element.items) == len(copy_cart.items)
+    assert create_expected_output(copy_cart) in captured.out
+
+
+@pytest.mark.parametrize("user_inputs", ["N", "n", "No", "no"])
+def test_cancel_logout_cart_with_multiple_elements(user_inputs, cart_with_multiple_elements, capsys):
+    copy_cart = copy.deepcopy(cart_with_multiple_elements)
+
+    with mock.patch('builtins.input', side_effect=user_inputs):
+        result = logout(cart=cart_with_multiple_elements)
+
+    # Capture the printed output
+    captured = capsys.readouterr()
+
+    assert result == False
+    assert len(cart_with_multiple_elements.items) == len(copy_cart.items)
+    assert create_expected_output(copy_cart) in captured.out
+
+
+@pytest.mark.parametrize("user_inputs", ["Y", "y", "Yes", "yes"])
+def test_logout_clear_cart_with_one_element(user_inputs, cart_with_one_element, capsys):
+    copy_cart = copy.deepcopy(cart_with_one_element)
+
+    with mock.patch('builtins.input', side_effect=user_inputs):
+        result = logout(cart=cart_with_one_element)
+
+    # Capture the printed output
+    captured = capsys.readouterr()
+
+    assert result == True
+    assert len(cart_with_one_element.items) == 0
+    assert create_expected_output(copy_cart) in captured.out
+
+
+@pytest.mark.parametrize("user_inputs", ["Y", "y", "Yes", "yes"])
+def test_logout_clear_cart_with_multiple_elements(user_inputs, cart_with_multiple_elements, capsys):
+    copy_cart = copy.deepcopy(cart_with_multiple_elements)
+
+    with mock.patch('builtins.input', side_effect=user_inputs):
+        result = logout(cart=cart_with_multiple_elements)
+
+    # Capture the printed output
+    captured = capsys.readouterr()
+
+    assert result == True
+    assert len(cart_with_multiple_elements.items) == 0
+    assert create_expected_output(copy_cart) in captured.out
+
